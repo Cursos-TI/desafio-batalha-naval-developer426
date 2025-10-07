8fcd4c42741b329c6c6746862021157673b70b8d
developer426 <matheeusnevess@gmail.com>
2025-10-07 17:08:56 +0000

    Erro de depuração ajustado e projeto finalizado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -19,11 +19,11 @@ int main(){
     // Determinação das coordenadas dos navios
     
     // Coordenadas do navio 1
-    int linha_navio1 = 6;
-    int coluna_navio1 = 7;
+    int linha_navio1 = 2;
+    int coluna_navio1 = 3;
 
     // Coordenadas navio 2
-    int linha_navio2 = 1;
+    int linha_navio2 = 5;
     int coluna_navio2 = 6;
 
     // Validação dos limites na horizontal para o navio 1
@@ -41,7 +41,7 @@ int main(){
     // Posicionamento do navio 1 na horizontal
     for (i = 0; i < tamanho_navio; i++){
         // Verifica a sobreposição
-        if(tabuleiro[linha_navio1][coluna_navio1 + i] = navio){
+        if (tabuleiro[linha_navio1][coluna_navio1 + i] == navio){
             printf("Erro: sobreposição detectada ao posicionar o navio horizontal.\n");
             return 1; // Finaliza o programa em caso de erro
         }
@@ -49,38 +49,35 @@ int main(){
     }
 
     // Posicionamento do navio 2 na vertical
-    for (i = ; i < tamanho_navio; i++){
+    for (i = 0; i < tamanho_navio; i++){
         // Verifica a sobreposição
-        if (tabuleiro[linha_navio2 + i][coluna_navio2] = navio){
+        if (tabuleiro[linha_navio2 + i][coluna_navio2] == navio){
             printf("Erro: sobreposição detectada ao posicionar o navio vertical.\n");
             return 1;
         }
+        tabuleiro[linha_navio2 + i][coluna_navio2] = navio;
     }
-    tabuleiro[linha_navio2 + i][coluna_navio2] = navio;
 
+    // Exibição do tabuleiro
+    printf("\n--- TABULEIRO BATALHA NAVAL ---\n");
 
+    // Cabeçalho das colunas
+    printf("   "); //espaço para alinhar com a coluna dos números
+    for (j = 0; j < tamanho_tabuleiro; j++){
+        printf("%2d ", j + 1);
+    }
+    printf("\n");
 
+    // Linhas numeradas 1 ao 10
+    for (i = 0; i < tamanho_tabuleiro; i++){
+        printf("%2d ", i + 1); // Número da linha a esquerda
+        for (j = 0; j < tamanho_tabuleiro; j++){
+            printf("%2d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nLegenda: 0 = água | 3 = Návio\n");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 
 }
